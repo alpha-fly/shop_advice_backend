62304eee672a4b84caf18731287641d8185e1977
alpha-fly <placetobe@naver.com>
2022-06-10 20:40:59 +0900

    1st commit : 로그인 기능 (암호화 이전)

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose
+    .connect('mongodb://test:test@3.34.42.87:27017/admin', {
+        dbName: 'shop_advice',
+        ignoreUndefined: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+const app = express();
+const port = 3000;
+
+const articleRouter = require('./routes/article');
+const userRouter = require('./routes/user');
+
+const requestMiddleware = (req, res, next) => {
+    // ** app.use (미들웨어)의 순서 중요!!
+    console.log('Request URL:', req.originalUrl, ' - ', new Date());
+    next();
+};
+
+app.use(express.static('static'));
+app.use(express.json()); //JSON 데이터 parsing middleware
+app.use(express.urlencoded());
+app.use(requestMiddleware); // 콘솔에 request 들어오면 url이랑 날짜 찍어주는.
+
+app.use('/api/article', [articleRouter]);
+app.use('/api/user', [userRouter]);
+
+app.get('/', (req, res) => {
+    //여기가 Router. 미들웨어와 유사하게 생김 (일종의 미들웨어다)request와 response
+    res.send('hello world');
+});
+
+app.listen(port, () => {
+    console.log(port, '포트로 서버가 켜졌어요!');
+});
---
+++ static/api.js
@@ -0,0 +1,309 @@
+// 게시글 조회
+function getArticles(callback) {
+    $('#articleList').empty();
+    $.ajax({
+        type: 'GET',
+        url: `/api/article`,
+
+        success: function (response) {
+            callback(response['articles']);
+        },
+    });
+}
+
+// 게시글 상세 조회
+function getArticleDetail(articleId, callback) {
+    $.ajax({
+        type: 'GET',
+        url: `/api/article/${articleId}`,
+
+        success: function (response) {
+            callback(response.article);
+        },
+    });
+}
+
+// 게시글 작성
+function writeArticle() {
+    let articleId;
+    let date = new Date();
+    let title = $('#title').val();
+    let content = $('#content').val();
+
+    $.ajax({
+        type: 'POST',
+        url: `/api/article/new_article`,
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+        data: {
+            date,
+            user,
+            title,
+            content,
+        },
+        error: function (xhr, status, error) {
+            if (xhr.status == 401) {
+                alert('로그인이 필요한 기능입니다.');
+            }
+
+            if (xhr.status == 400) {
+                alert('제목과 내용을 입력해주세요');
+            }
+
+            window.location.reload();
+        },
+        success: function (response) {
+            alert(response['message']);
+            window.location.href = '/';
+        },
+    });
+}
+
+// 게시글 삭제
+function deleteArticle(articleId) {
+    $.ajax({
+        type: 'DELETE',
+        url: `/api/article/${articleId}`,
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+        error: function (xhr, status, error) {
+            if (xhr.status == 400 || xhr.status == 401) {
+                alert('작성자만 삭제할 수 있습니다.');
+            }
+        },
+        success: function (response) {
+            alert(response['message']);
+            window.location.href = '/';
+        },
+    });
+}
+
+// 게시글 수정 페이지로 이동
+function goRevisePage(articleId) {
+    $.ajax({
+        type: 'GET',
+        url: `/api/article/revise_check/${articleId}`,
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+        error: function (xhr, status, error) {
+            if (xhr.status == 400 || xhr.status == 401) {
+                alert('작성자만 수정할 수 있습니다.');
+            }
+        },
+        success: function (response) {
+            window.location.href = 'revise.html?articleId=' + articleId;
+        },
+    });
+}
+
+// 게시글 수정
+function reviseArticle(articleId) {
+    let title = $('#title').val();
+    let content = $('#content').val();
+
+    $.ajax({
+        type: 'PUT',
+        url: `/api/article/${articleId}`,
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+        data: {
+            title,
+            content,
+        },
+        error: function (xhr, status, error) {
+            if (xhr.status == 401) {
+                alert('작성자만 수정할 수 있습니다');
+            }
+
+            if (xhr.status == 400) {
+                alert('제목과 내용을 입력해주세요');
+            }
+
+            window.location.reload();
+        },
+        success: function (response) {
+            alert(response['message']);
+            window.location.href = '/';
+        },
+    });
+}
+
+// 댓글 조회
+function getCommentList(articleId, callback) {
+    $.ajax({
+        type: 'GET',
+        url: `/api/article/comment/${articleId}`,
+
+        success: function (response) {
+            callback(response.comments);
+        },
+    });
+}
+
+// 댓글 작성
+function comment() {
+    let location;
+    let date = new Date();
+    let mention = $('#comment').val();
+
+    $.ajax({
+        type: 'POST',
+        url: `/api/article/comment/${articleId}`,
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+        data: {
+            date,
+            mention,
+            location,
+        },
+        error: function (xhr, status, error) {
+            if (xhr.status == 401) {
+                alert('로그인 해주세요.');
+            }
+
+            if (xhr.status == 400) {
+                alert('댓글 내용을 입력해주세요.');
+            }
+
+            window.location.reload();
+        },
+        success: function (response) {
+            alert(response['message']);
+            window.location.reload();
+        },
+    });
+}
+
+// 댓글 삭제
+function deleteComment(commentId) {
+    $.ajax({
+        type: 'DELETE',
+        url: `/api/article/comment/${commentId}`,
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+        error: function (xhr, status, error) {
+            if (xhr.status == 401) {
+                alert('작성자만 삭제할 수 있습니다.');
+            }
+        },
+        success: function (response) {
+            alert(response['message']);
+            window.location.reload();
+        },
+    });
+}
+
+// 댓글 수정
+function reviseComment(commentId) {
+    let mention = $('#mention').val();
+
+    $.ajax({
+        type: 'PUT',
+        url: `/api/article/comment/${commentId}`,
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+        data: {
+            mention,
+        },
+        error: function (xhr, status, error) {
+            if (xhr.status == 401) {
+                alert('작성자만 수정할 수 있습니다.');
+            }
+
+            if (xhr.status == 400) {
+                alert('댓글 내용을 입력해주세요.');
+            }
+            window.location.reload();
+        },
+        success: function (response) {
+            alert(response['message']);
+            window.location.reload();
+        },
+    });
+}
+
+// 로그인 체크
+function getSelf(callback) {
+    $.ajax({
+        type: 'GET',
+        url: '/api/user/me',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+        success: function (response) {
+            callback(response.user);
+        },
+        error: function (xhr, status, error) {
+            if (xhr.status == 401) {
+                alert('로그인이 필요합니다.');
+            } else {
+                localStorage.clear();
+                alert('알 수 없는 문제가 발생했습니다. 관리자에게 문의하세요.');
+            }
+            window.location.href = '/';
+        },
+    });
+}
+
+// 로그인
+function sign_in() {
+    let id = $('#inputid').val();
+    let password = $('#inputPassword').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/user/auth',
+        data: {
+            id: id,
+            password: password,
+        },
+        success: function (response) {
+            localStorage.setItem('token', response.token);
+            window.location.replace('/articles.html');
+        },
+        error: function (error) {
+            customAlert(error.responseJSON.errorMessage);
+        },
+    });
+}
+
+// 회원가입
+function sign_up() {
+    const id = $('#inputid').val();
+    const nickname = $('#inputNickname').val();
+    const password1 = $('#inputPassword1').val();
+    const password2 = $('#inputPassword2').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/user/new_user',
+        data: {
+            id: id,
+            nickname: nickname,
+            password: password1,
+            confirmPassword: password2,
+        },
+        success: function (response) {
+            customAlert('회원가입을 축하드립니다!', function () {
+                window.location.replace('/');
+            });
+        },
+        error: function (error) {
+            customAlert(error.responseJSON.errorMessage);
+        },
+    });
+}
+
+// 로그아웃
+function signOut() {
+    localStorage.clear();
+    alert('로그아웃 하셨습니다.');
+    window.location.href = '/';
+}
