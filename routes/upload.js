f9e29f93b81907fb225b22f0c2296b7da4fdd3c8
alpha-fly <placetobe@naver.com>
2022-06-14 19:31:01 +0900

    이미지 파일 경로 추출

--- routes/upload.js
+++ routes/upload.js
@@ -18,7 +18,10 @@ router.get("/", (req, res) => {
   res.send("upload");
 });
 
-router.post("/upload", upload.single("image"), (req, res) => {
+router.post("/upload", upload.single("image"), (req, res) => {  
+  // 파일 상대경로(프로젝트 폴더 내의 경로)를 imagePath 변수에 저장합니다.
+  const imagePath = req.file["path"]
+  console.log(imagePath) 
   res.send("upload:" + req.file.filename); 
 });
 
