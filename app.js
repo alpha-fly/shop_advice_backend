8a604965c5b0c3310d22343af091b7ec2cbcb1c8
alpha-fly <placetobe@naver.com>
2022-06-15 11:27:00 +0900

    CORS 설정2

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const fs = require('fs'); // multer 때문에. 파일시스템 접근.
 
+const domains = ['http://shop-advice.s3-website.ap-northeast-2.amazonaws.com'];
+const corsOptions = {
+  origin: function(origin, callback){
+  	const isTrue = domains.indexOf(origin) !== -1;
+    callback(null, isTrue);
+  }
+  ,
+  credentials: true
+}
+
+app.use(cors(corsOptions));
+
+// app.use(cors({
+//     origin: 'http://shop-advice.s3-website.ap-northeast-2.amazonaws.com/', // 출처 허용 옵션, 아스테리스크로 놓지 말고 frontend 출처로 변경할 것!
+//     credential: 'true' // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+// }));
+
 mongoose
     .connect(process.env.MONGODB, {
         dbName: 'shop_advice',
@@ -35,23 +52,6 @@ app.use(express.static('static'));
 app.use(express.json()); //JSON 데이터 parsing middleware
 app.use(express.urlencoded());
 app.use(requestMiddleware); // 콘솔에 request 들어오면 url이랑 날짜 찍어주는.
-
-const domains = ['http://shop-advice.s3-website.ap-northeast-2.amazonaws.com'];
-const corsOptions = {
-  origin: function(origin, callback){
-  	const isTrue = domains.indexOf(origin) !== -1;
-    callback(null, isTrue);
-  }
-  ,
-  credentials: true
-}
-
-app.use(cors(corsOptions));
-
-// app.use(cors({
-//     origin: 'http://shop-advice.s3-website.ap-northeast-2.amazonaws.com/', // 출처 허용 옵션, 아스테리스크로 놓지 말고 frontend 출처로 변경할 것!
-//     credential: 'true' // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
-// }));
 
 app.use('/api/user', [userRouter]);
 app.use('/api/article', [articleRouter]);
